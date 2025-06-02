f7213e0caa9b653700752497a4adc47192ee39b9
AlexRook777 <alexrook777@gmail.com>
2025-06-02 07:18:40 +0300

    1

--- second.py
+++ second.py
@@ -1,9 +1,22 @@
-import random
+# Встановлюємо ціни на продукти
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per_coffee_pack = 4.42
 
-message = "Hello world!"
-i = random.randint(0, 10)
-y = random.randint(i, len(message))
+# Кількість кожного продукту
+num_croissants = int(input("Введіть кількість круасанів: "))
+num_glasses = int(input("Введіть кількість склянок: "))
+num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
 
-print(message)
-print(f"i: {i}, y: {y}")
-print(message[i:y])
+# Обчислення загальної вартості
+total_cost = num_croissants * price_per_croissant + \
+             num_glasses * price_per_glass + \
+             num_coffee_packs * price_per_coffee_pack
+
+# Визначаємо кількість повних доларів і центів
+total_dollars = float(total_cost)
+total_cents = int(total_cost * 100)
+
+# Вивід результату
+print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+print(f"Загальна вартість у центах: {total_cents} центів")
