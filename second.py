227c409e6a46cc24af2d41581504365cb787e9c6
AlexRook777 <alexrook777@gmail.com>
2025-06-04 08:04:39 +0300

    1

--- second.py
+++ second.py
@@ -1,9 +1,15 @@
-pool = 1000
-quantity = int(input("Enter the number of mailings: "))
-try:
-    chunk = int(pool/quantity)
-except :
-    print('Divide by zero completed!')
-    chunk = 1
+from datetime import datetime
 
-print(f'Each mailing will have {chunk} items.')
+def calculate_person_age(date_of_birth_str: str) -> int:
+    date_of_birth = datetime.strptime(date_of_birth_str, "%Y-%m-%d")  # str parse time
+    today = datetime.today()
+
+    age = today.year - date_of_birth.year
+
+    if (date_of_birth.month, date_of_birth.day) > (today.month, today.day):
+        age -= 1
+
+    return age
+
+print(calculate_person_age("1974-08-19"))  # 25
+print(calculate_person_age("1974-02-19"))  # 1
